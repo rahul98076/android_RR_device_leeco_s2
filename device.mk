aa3267a8ee32548ccebabc4af49cb8a9516ec9af
rahul98076 <rahu98076@gmail.com>
2018-05-19 10:08:19 +0000

    trial and erro First commit

--- device.mk
+++ device.mk
@@ -22,7 +22,7 @@ $(call inherit-product-if-exists, vendor/leeco/s2/s2-vendor.mk)
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-rr
 
 # Screen density
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
